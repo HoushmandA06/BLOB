2432cb61e444beb64b4875647582c1e7a883b3a3
Ali Houshmand <ali.houshmand06@gmail.com>
2014-06-03 12:36:22 -0400

    SpriteKit parallax

---
+++ Blob/Blob/BLBMyScene.h
@@ -0,0 +1,13 @@
+//
+//  BLBMyScene.h
+//  Blob
+//
+
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BLBMyScene : SKScene
+
+@end
---
+++ Blob/Blob/BLBViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLBViewController.h
+//  Blob
+//
+
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface BLBViewController : UIViewController
+
+@end
